80f4aa637e7d9ae6771bdcac337e839dbfa637e5
Pratik <cpratik025@gmail.com>
2026-04-12 23:09:05 -0400

    Added java event listener

--- password-generator-project/index.js
+++ password-generator-project/index.js
@@ -1,8 +1,8 @@
-firstPassword=document.getElementById("first-password");
-secondPassword=document.getElementById("second-password");
-generateBtn=document.getElementById("generate-btn");
-characters=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '[', '}', ']', ',', '|', ':', ';', '<', '>', '.', '?']
-passwordLength=document.getElementById("input-number");
+let firstPassword=document.getElementById("first-password");
+let secondPassword=document.getElementById("second-password");
+const generateBtn=document.getElementById("generate-btn");
+let characters=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2]', ',', '|', ':', ';', '<', '>', '.', '?'];
+let passwordLength=document.getElementById("input-number");
 
 function generatePassword(){
     let isPasswordLengthValid=passwordLengthValue();
@@ -25,7 +25,7 @@ function generatePassword(){
 }
 
 function passwordLengthValue(){
-    passwordLengthVal=document.getElementById("input-number").value;
+    let passwordLengthVal=document.getElementById("input-number").value;
     if (passwordLengthVal < 8 || passwordLengthVal > 16){
         return false
     }
@@ -33,3 +33,5 @@ function passwordLengthValue(){
         return true
     }
 }
+
+generateBtn.addEventListener("click", generatePassword);
